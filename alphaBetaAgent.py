f6e5d6ea7aa972a3b11b54753542d05d3c3b961e
guygolran <guy.golran@mail.huji.ac.il>
2018-08-08 19:30:36 +0300

    AlphaBeta improvements

--- alphaBetaAgent.py
+++ alphaBetaAgent.py
@@ -1,16 +1,20 @@
 import numpy as np
 import Evaluation as eval
+from abalone import Action
+
+
+def eval_fn(game_state, agent_index):
+    return 5 * eval.lost_marbles(game_state, agent_index) + eval.dist_from_center(game_state, agent_index)
+
 
 class Agent():
     """
     Minimax agent with alpha-beta pruning
     """
-    DEPTH = 2
-    
-    def evaluation_function(self, game_state, agent_index):
-        return (100 * eval.win_or_lose(game_state, agent_index)) + eval.lost_marbles(game_state, agent_index) + \
-               eval.dist_from_center(game_state, agent_index) + eval.own_marbles_grouping(state, agent_index) - \
-               eval.opposing_marbles_grouping(state, agent_index)
+    def __init__(self, depth, evaluation_function=eval_fn):
+        self.depth = depth
+        self.evaluation_function = evaluation_function
+
     
     def get_action(self, game_state, agent_index):
         """
@@ -19,10 +23,11 @@ class Agent():
         legal_moves = game_state.get_legal_actions(agent_index)
         if len(legal_moves) == 0:
             return Action.STOP
-        scores = [self.alpha_beta_search(-agent_index, game_state.generate_successor(0, action), DEPTH, -np.inf, np.inf) for action in legal_moves]
+        scores = [self.alpha_beta_search(-agent_index, game_state.generate_successor(0, action), self.depth, -np.inf, np.inf) for action in legal_moves]
         best_score = max(scores)
         best_indices = [index for index in range(len(scores)) if scores[index] == best_score]
         return legal_moves[np.random.choice(best_indices)]
+
 
     def alpha_beta_search(self, agent_index, game_state, depth, alpha, beta):
         """
